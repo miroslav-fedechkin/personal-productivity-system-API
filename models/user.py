6779e7ea4de707e0a5933ea17c030e3a2097a525
Miroslav <miroslav-dev@yandex.ru>
2026-04-08 21:51:46 +0200

    tablename add

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@ from sqlalchemy import String, Integer, Column
 from core.database import Base
 
 class User(Base):
+    __tablename__ = 'users'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String, unique=True)
